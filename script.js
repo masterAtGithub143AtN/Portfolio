fa8cf780afcd03a6acd0a7c38b972a983d6adc8e
saket suman <12mdpur@gmail.com>
2024-03-22 19:46:39 +0530

    second edition

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+function sendEmail(){
+    Email.send({
+        SecureToken: "bcf1ae0b-2e4d-4251-ae5e-10cb803b4cf8",
+        To : 'saketsuman1013@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "Someone has Contected you",
+        Body : "Namse: "+ document.getElementById("name").value
+        +"<br> Email: "+document.getElementById("email").value
+        +"<br> Message: "+document.getElementById("massage")
+    }).then(
+      message => alert("Message sent succesfully")
+    );
+}
